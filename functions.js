a71561a72e58f4c3930ae7502265bc3550a2423a
Merchuli <merchu.liang@gmail.com>
2022-05-20 17:06:10 +0800

    Add samples for toBeNull and toBeUndefined

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 const functions = {
   add: (num1, num2) => num1 + num2,
+  isNull: () => null,
+  isUndefined: () => undefined,
 };
 
 module.exports = functions;
--- functions.spec.js
+++ functions.spec.js
@@ -1,9 +1,21 @@
 const functions = require('./functions');
 
+// matcher: toBe
 test('Add 2 and 3 equal to 5', () => {
   expect(functions.add(2, 3)).toBe(5);
 });
 
+// matcher: not
 test('Add 2 and 3 not equal to 6', () => {
   expect(functions.add(2, 3)).not.toBe(6);
+});
+
+// matcher: toBeNull
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+// matcher: toBeUndefined
+test('Should be undefined', () => {
+  expect(functions.isUndefined()).toBeUndefined();
 });
